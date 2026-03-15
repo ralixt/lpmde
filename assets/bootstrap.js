548d82b17464a808a56648afb3d20e6af05c8555
ralixt <98614258+ralixt@users.noreply.github.com>
2026-03-15 08:11:20 +0100

    fix: résolution des conflits Git non résolus sur les configurations actives

--- assets/bootstrap.js
+++ assets/bootstrap.js
@@ -1,11 +1,5 @@
 import { startStimulusApp } from '@symfony/stimulus-bundle';
 
 const app = startStimulusApp();
-<<<<<<< HEAD
-import { startStimulusApp } from '@symfony/stimulus-bundle';
-
-const app = startStimulusApp();
-=======
->>>>>>> 2a2a3044f9adead3da88b05626bcae0a87f58ad4
 // register any custom, 3rd party controllers here
 // app.register('some_controller_name', SomeImportedController);
